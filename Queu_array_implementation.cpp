9ac2a7378f4e9936816fd91d541ca462d950712a
Muh Yusuf <yusufmuhyusuh@gmail.com>
2025-08-31 01:30:28 +0700

    feat: menambahkan implementasi queue dengan array

---
+++ Queu_array_implementation.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+#include <cstddef>
+/**
+* @brief Queu mempunya beberapa operasi dasar yaitu:
+*        -  enqueue adalah operasi untuk menambah element ke queue
+*        -  dequeu adalah operasi untuk menghapus element dari queue
+*        -  getFront/peek mengambil element paling depan tanpa menghapus
+*        -  gerRear mengambil elemnt paling belakang tanpa menghapus
+*/
+template <typename type>
+class Array{
+    private:
+        std::size_t capacity;
+        int size;
+        type* arr;
+    public:
+        //default constructor
+        Array(){
+            this->capacity = 1;
+            this->size = 0;
+            this->arr = new type[capacity];
+        }
+        //Constructor
+        Array(std::size_t capacity){
+            this->capacity = capacity;
+            this->size = 0;
+            this->arr = new type[capacity];
+        }
+        //copy constructor
+        Array(const Array& other){
+            size = other.size;
+            arr = new type[capacity];
+            for(int i = 0;i < size;i++){
+                arr[i] = other.arr[i];
+            }
+        }
+        //copy assignment / operator overloading
+        Array& operator=(const Array& other){
+            if(this != &other){ //objeck saat ini tidak sama dengan objek yg ingin copy
+                size = other.size;
+                arr = new type[capacity];
+                for(size_t i = 0;i < size;i++){
+                    arr[i] = other.arr[i];
+                }
+            }
+            return *this; //kembalikan objeck saat ini
+        }
+        //desctructor
+        ~Array(){
+            clear();
+        }
+    public: //abstraksi getter
+        int get_size()const noexcept{
+            return this->size;
+        }
+        int get_capacity()const noexcept{
+            return this->capacity;
+        }
+        bool is_full()const noexcept{
+            if(size == capacity){
+                return true;
+            }
+            return false;
+        }
+        bool is_empty()const noexcept{
+            if(size == 0){
+                return true;
+            }
+            return false;
+        }
+        type getFront()const noexcept{
+            return this->arr[0];
+        }
+        type getRear()const noexcept{
+            return this->arr[size - 1];
+        }
+    public: //abstraksi setter
+        void enqueu(const type& data){
+            arr[size] = data;
+            size++;
+        }
+        void dequeu(){
+            for(size_t i = 1;i < size;i--){
+                arr[i - 1] = arr[i];
+                //menggeser arr[0] menjadi element terakhir
+            }
+            size--;
+        }
+    public: //abstraksi dynamic array
+        void grow_array()noexcept{
+            type* temp = new type[capacity * 2];
+            capacity *= 2;
+            for(size_t i = 0;i < size;i++){
+                temp[i] = arr[i];
+            }
+            delete[] arr;
+            arr = temp;
+        }
+    public: // setter destructor & print
+        void clear(){  
+            int n_size = get_size();
+            for(size_t i = 0;i < n_size;i++){
+                arr[i] = 0;
+            }
+            size = 0;
+        }
+        void print()const noexcept{
+            for(size_t i = 0;i < size;i++){
+                std::cout << arr[i] <<  " ";
+            }
+            std::cout << std::endl;
+        }
+
+};
+int main(){
+    // Your code here
+    return 0;
+}
