77fd198c897f3caaa52a6f3e09faf18f6ec52fbf
Muh Yusuf <yusufmuhyusuh@gmail.com>
2025-09-01 21:11:38 +0700

    feat: menambahkan impl use linked list

---
+++ Queue_linked_list.cpp
@@ -0,0 +1,142 @@
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+template <typename type>
+class Linked_list{
+    private: //abstraksi linked list
+        struct Node{
+            type data;
+            Node* next;
+            //constructor
+            Node(const type& data){
+                this->data = data;
+                this->next = nullptr;
+            }
+        };
+    private: //abstraksi data member Queue
+        int size;
+        Node* front;
+        Node* rear;
+    public:
+        //default constructor
+        Linked_list(){
+            this->size = 0;
+            this->front = nullptr;
+            this->rear = nullptr;
+        }
+        ~Linked_list(){
+            clear();
+        }
+    public: //abstraksi getter
+        int get_size()const noexcept{
+            return this->size;
+        }
+        type get_front()const{
+            if(is_empty()){
+                throw std::runtime_error("Queue is empty"); 
+            }
+            return front->data;
+        }
+        bool is_last()const noexcept{
+            if(front->next == nullptr && rear->next == nullptr){
+                return true;
+            }
+            return false;
+        }
+        bool is_first()const noexcept{
+            if(front == nullptr && rear == nullptr){
+                return true;
+            }
+            return false;
+        }
+        bool is_empty()const noexcept{
+            if(size == 0){
+                return true;
+            }
+            return false;
+        }
+        type get_peek()const{
+            if(is_empty()){
+                throw std::runtime_error("Queue is empty"); ;
+            }
+            return rear->data;
+        }
+    public:
+        void enqueue(const type& data){
+            if(is_first()){
+                //buat pdecrementointer penunjuk yang menunjuk node baru
+                Node* baru = new Node(data);
+                //hubungkan pointer penunjuk dengan head
+                front = rear = baru;
+                //increment size
+                size++;  
+            }else{
+                //buat pointer penunjuk yang menunjuk node baru
+                Node* baru = new Node(data);
+                //hubungkan pointer penunjuk dengan head
+                rear = rear->next = baru;
+                //increment size
+                size++;
+            }
+        }
+        void dequeue(){
+            if(is_empty()){
+                throw std::runtime_error("Queue is empty");
+            }
+            if(is_last()){
+                Node* temp = front;
+                front = rear = nullptr;
+                delete temp;
+                size--;
+            }else{
+                //buat pointer penunjuk curr
+                Node* temp = front;
+                //gerakakn pointer head sehingga menunjuk node selanjut nya
+                front = front->next;
+                //hapus node lama
+                delete temp;
+                //decrement size
+                size--;
+            }
+        }
+    public: //abstraksi print dan clear
+        void clear()noexcept{
+            while(front != nullptr){
+                Node* temp = front;
+                front = front->next;
+                delete temp; //hapus node
+            }
+            size = 0;
+        }
+        void print()const noexcept{
+            if(is_empty()){
+                std::cout << "Queue is empty" << std::endl;
+                EXIT_SUCCESS;
+            }else{
+                Node* curr = front;
+                while(curr != nullptr){
+                    std::cout << curr->data << " ";
+                    curr = curr->next;
+                }
+                std::cout << std::endl;
+            }
+        }
+};
+int main(){
+    Linked_list<int>Queue1;
+    Queue1.enqueue(1);
+    Queue1.enqueue(2);
+    Queue1.enqueue(3);
+    Queue1.print();
+    std::cout << "ini front: " << Queue1.get_front() << std::endl;
+    std::cout << "ini rear: " << Queue1.get_peek() << std::endl;
+    std::cout << "ini size: " << Queue1.get_size() << std::endl;
+    std::cout << "ini proses dequeue "<< std::endl;
+    Queue1.dequeue();
+    Queue1.dequeue();
+    Queue1.dequeue();
+    Queue1.print();
+    // Your code here
+    std::cin.get();
+    return 0;
+}
