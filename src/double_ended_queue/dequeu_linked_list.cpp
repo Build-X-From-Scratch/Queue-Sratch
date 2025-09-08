97fec55e3d58d5579587936438e27bee1c99ae76
Muh Yusuf <yusufmuhyusuh@gmail.com>
2025-09-08 13:38:11 +0700

    feat: memakai doubly linked list & menambahkan method swap

--- src/double_ended_queue/dequeu_linked_list.cpp
+++ src/double_ended_queue/dequeu_linked_list.cpp
@@ -17,19 +17,20 @@
  * get_size -> count element on queue
  */
 #include <iostream>
-#include <deque>
 #include <stdexcept>
-std::deque<int>s;
+static int cnt = 0; 
 template <typename type>
 class Linked_List{
     private:
         struct Node{
             type data;
             Node* next;
+            Node* prev;
             //constructor Node
             Node(const type& data){
                 this->data = data;
                 this->next = nullptr;
+                this->prev = nullptr;
             }
         };
     private:
@@ -42,6 +43,55 @@ class Linked_List{
             this->front = nullptr;
             this->rear = nullptr;
             this->size = 0;
+        }
+        Linked_List(const Linked_List& others){
+            size = others.size;
+            front = others.front;
+            rear = others.rear;
+            if(others.front == nullptr){
+                return;
+            }
+            front = new Node(others.front->data);
+            Node* curr = front;
+            Node* temp = others.front->next;
+            while(temp != nullptr){
+                Node* new_node = new Node(temp->data); //buat node baru
+                curr->next = new_node; //ponter next adalah new_node
+                new_node->prev = curr; //prev adalah curr saat ini
+                curr = new_node; //majukan pointer curr
+                temp = temp->next; //majukan pointer temp
+                
+            }
+            rear = curr;
+        }
+        Linked_List& operator=(const Linked_List& others){
+            if(this == &others){
+                return *this;
+            }
+            clear();
+            if(!others.front){ //linked list is empty
+                return nullptr;
+            }
+            front = others.front;
+            rear = others.rear;
+            size = others.size;
+            front = new Node(others.front->data);
+            Node* curr = front;
+            Node* temp = others.front->next;
+            while(temp != nullptr){
+                //buat node baru di heap
+                Node* new_node = new Node(temp->data);
+                //node setelah curr sekarang adalah new_node
+                curr->next = new_node;
+                //node sebelum prev adalah curr
+                new_node->prev = curr;
+                //majukan pointer curr,sekarang curr menunjuk new_node
+                curr = new_node;
+                // majukan pointer next temp
+                temp = temp->next;
+            }
+            rear = curr;
+            return *this;
         }
         ~Linked_List(){
             clear();
@@ -56,34 +106,79 @@ class Linked_List{
         int get_size()const noexcept{
             return size;
         }
-        bool is_empty(){
+        bool is_empty(){ //refactor
             if(size == 0){
-               return true; 
+                return true;
             }
             return false;
         }
     public://abstraksi setter
         void push_back(const type& data){
-            if(is_empty()){
-                Node* new_node = new Node(data);
-                front = rear = new_node;
-            }
             Node* new_node = new Node(data);
-            rear = rear->next = new_node;
+            if(is_empty()){
+                front = rear = new_node;
+                front->prev = nullptr; //update     prev ->opsional 
+                size++;
+            }else{
+                rear->next = new_node; //hubunngkan new node dengan rear
+                new_node->prev = rear; //prev new_node skrg adalah rear
+                rear = new_node; //rear sekarang adalah new node
+                size++; //increment size
+            }
+        }
+    public:
+        void swap(Linked_List& others){
+           //swap head
+           Node* tempFront = front;
+           front = others.front;
+           others.front = tempFront;
+           //swap tail
+           Node* tempRear = rear; 
+           rear = others.rear;
+           others.rear = tempRear;
+           //swap size
+           int tempSize = size;
+           size = others.size;
+           others.size = tempSize;
         }
         void push_front(const type& data){
             if(is_empty()){
                 Node* new_node = new Node(data);
                 front = rear = new_node;
+                size++;
+            }else{
+                Node* new_node = new Node(data);
+                new_node->next = front;
+                new_node->prev = nullptr;
+                front = new_node;
+                size++;
             }
-            Node* new_node = new Node(data);
-            front = new_node->next = front;
         }
         void pop_back(){
             if(is_empty()){
                 throw std::runtime_error("Queue is empty");
             }
-            
+            if(front == nullptr){
+                front = rear = nullptr;
+            }else{
+                Node* temp = front;
+                front = front->next;
+                front->prev = nullptr;
+                delete temp;
+            }
+        }
+        void pop_front(){
+            if(is_empty()){
+                throw std::runtime_error("Queue is empty");
+            }
+            if(rear == nullptr){
+                return;
+            }else{
+                Node* temp = rear;
+                rear = rear->prev;
+                rear->next = nullptr;
+                delete temp; 
+            }
         }
     public:
         void clear(){
@@ -94,13 +189,41 @@ class Linked_List{
             }
         }
         void print(){
-
+            cnt++;
+           // std::cout << "operasi ke " << cnt << std::endl;
+           // std::cout << "Object yang memanggil: " << this << std::endl;
+            Node* temp = front;
+            while(temp != nullptr){
+                std::cout << temp->data << " ";
+                temp = temp->next;
+            }
+            std::cout << std::endl;
         }
 };
 int main(){
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(NULL);
+    Linked_List<int>dequeu;
+    dequeu.push_back(1);
+    dequeu.push_back(2);
+    dequeu.push_back(3);
+    dequeu.push_back(4);
+    dequeu.push_back(5); 
+    dequeu.print(); // 1 2 3 4 5
+    dequeu.push_front(6);
+    dequeu.print();//6 1 2 3 4 5
+    Linked_List<int>dequeue_temp = dequeu;
+    dequeue_temp.print();
+    Linked_List<int>dequeu1;
+    dequeu1.push_front(10);
+    dequeu1.push_front(12);
+    dequeu1.push_front(13);
+    std::cout << "sebelum swap" << std::endl;
+     dequeu1.print();
+    dequeu1.swap(dequeue_temp);
+    std::cout << "sesudah swap" << std::endl;
+    dequeu1.print();
+    dequeue_temp.print();
     // Your code here
-    std::cin.get();
     return 0;
 }
